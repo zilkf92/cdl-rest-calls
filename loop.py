7829b42952cce541d4ac21896e9751ada615d4c1
Felix Zilk <zilk.felix@gmail.com>
2021-12-22 13:38:00 +0100

    Add if statement for empty queue

--- loop.py
+++ loop.py
@@ -6,46 +6,48 @@ task = rest_calls.getexp_fromqueue(
     "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432")
 # if "detail":"Invalid token." or {"detail":"Invalid token header. No credentials provided."}
 
-print(task["experimentId"])
+# print(task["experimentId"])
+if "experimentId" in task:
+    experimentId = task["experimentId"]
+    rest_calls.poststatus_running(
+        "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", experimentId)
 
-experimentId = task["experimentId"]
-rest_calls.poststatus_running(
-    "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", experimentId)
+    # Here: execute task
 
-# execute task
+    # Here: retrieve result
 
-# retrieve result
-
-result = json.dumps({
-    "experiment": experimentId,
-    # "startTime": "2021-12-21T17:16:19.304243Z",
-    "totalCounts": 50000,
-    "numberOfDetectors": 4,
-    "singlePhotonRate": "1500.00",
-    "totalTime": 3,
-    "experimentData": {
-        "countratePerDetector": {
-            "d1": 123,
-            "d2": 123,
-            "d3": 456,
-            "d4": 123,
-            "d5": 123,
-            "d6": 456,
-            "d7": 123,
-            "d8": 123
-        },
-        "encodedQubitMeasurements": {
-            "c00": 0.123,
-            "c01": 0.56,
-            "c10": 0.123,
-            "c11": 0.34
+    result = json.dumps({
+        "experiment": experimentId,
+        # "startTime": "2021-12-21T17:16:19.304243Z",
+        "totalCounts": 50000,
+        "numberOfDetectors": 4,
+        "singlePhotonRate": "1500.00",
+        "totalTime": 3,
+        "experimentData": {
+            "countratePerDetector": {
+                "d1": 123,
+                "d2": 123,
+                "d3": 456,
+                "d4": 123,
+                "d5": 123,
+                "d6": 456,
+                "d7": 123,
+                "d8": 123
+            },
+            "encodedQubitMeasurements": {
+                "c00": 0.123,
+                "c01": 0.56,
+                "c10": 0.123,
+                "c11": 0.34
+            }
         }
-    }
-})
+    })
 
+    rest_calls.post_result(
+        "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", result)
 
-rest_calls.post_result(
-    "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", result)
+    rest_calls.poststatus_done(
+        "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", experimentId)
 
-rest_calls.poststatus_done(
-    "01a9b596c1b165fa4f15fbccad4092428dc47db76ad4e5dfe3bfee24b66bb432", experimentId)
+else:
+    pass
